f7f9f8a6fc823cb0b6c1c3dae8b1339409a8fc46
sandrakuc <san.kuc.96@gmail.com>
2017-10-31 13:10:37 +0100

    allegro test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <allegro.h>
+
+#define WIDTH 1600
+#define HEIGHT 900
 
 using namespace std;
 
-int main()
-{
-    cout << "No elo! druga zmiana" << endl;
-    return 0;
+int initiall(){
+
+return 0;
 }
+
+int main(){
+allegro_init();
+set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
+install_keyboard();
+install_mouse();
+while
+( (readkey()& 0xff) != 27);
+allegro_exit();
+return 0;
+}
+END_OF_MAIN()
