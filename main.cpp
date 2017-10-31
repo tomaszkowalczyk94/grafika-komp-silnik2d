44ddfa07176edead47fd5050b96f0332b1580b0e
sandrakuc <san.kuc.96@gmail.com>
2017-10-31 13:12:42 +0100

    funkcja inizacjująca

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 
 int initiall(){
-
-return 0;
-}
-
-int main(){
 allegro_init();
 set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
 install_keyboard();
 install_mouse();
+return 0;
+}
+
+int main(){
+initiall();
 while
 ( (readkey()& 0xff) != 27);
 allegro_exit();
